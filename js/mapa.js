f4a4b8702ea3289b14a218ee4e579c8c8784a011
niqueve <niquevellin@gmail.com>
2025-04-28 16:12:42 -0300

    async function

--- js/mapa.js
+++ js/mapa.js
@@ -1,6 +1,6 @@
 let mapInstance;
 
-function initMap() {
+async function initMap() {
     mapInstance = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -22.9194, lng: -42.8185 }, 
         zoom: 12,
